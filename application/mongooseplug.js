95225a9a6b9d36462eb2f6268247f5691ae277f9
Amal Mathew <>
2018-07-07 22:29:27 -0400

    Commit

--- application/mongooseplug.js
+++ application/mongooseplug.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser: true});
+mongoose.connect('mongodb://amal:players7@ds131551.mlab.com:31551/playersdb' 
+|| 'mongodb://localhost:27017/TodoApp',{useNewUrlParser: true});
 
 
 
